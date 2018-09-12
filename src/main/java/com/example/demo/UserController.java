6e0a2a19bab7a61ddb69c4d6e484e585e92559e7
claudio serrano <claudio.serrano@globant.com>
2018-09-12 00:57:46 -0300

    Add mini controller

---
+++ src/main/java/com/example/demo/UserController.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @RequestMapping(value = "/users", method = RequestMethod.GET)
+    public String getUser(){
+        return "hola";
+    }
+}
